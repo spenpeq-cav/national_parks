5c15ffa67743004c00e82d09ad10b0ffab4b1990
Spencer Pequegnat <spencer.peq@gmail.com>
2021-03-21 15:54:52 -0700

    Popular parks section homescreen, NPS API call to display data

--- frontend/src/components/PopularParks.js
+++ frontend/src/components/PopularParks.js
@@ -1,47 +1,98 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import ReactJson from 'react-json-view';
+import { Link } from 'react-router-dom'
 
 function PopularParks() {
+
+    const [popularParkData, setPopularParkData] = useState([])
+    const [loaded, setLoaded] = useState(false)
+
+    const popularParkCodes = 'grsm,grca,yose,romo,zion,yell'
+
+    const base_url = 'https://developer.nps.gov/api/v1'
+    const endpoint_url = '/parks?parkCode='+ popularParkCodes +'&api_key=' + process.env.REACT_APP_API_KEY
+    const url = base_url + endpoint_url
+
+    useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(res => {
+                setPopularParkData(res.data)
+                setLoaded(true)})
+            .catch(error => console.log(error))
+        
+        console.log('Fetching data...')
+    }, [])
     return (
         <div>
+            
+            { loaded ? (
+                <div>
+                    <div class="row">
+                        <div class="column">
+                            <div class="card">
+                                <h2>{popularParkData[0].name}</h2>
+                                <img src={popularParkData[0].images[1].url}/>
+                                <p>{popularParkData[0].description}</p>
+                                <Link className="button" to={`/explore/${popularParkData[0].parkCode}`}>View More Info</Link>   
+                            </div>
+                        </div>
 
-            <div class="row">
-                <div class="column">
-                    <div class="card">
-                        <h2>Card 1</h2>
+                        <div class="column">
+                            <div class="card">
+                                <h2>{popularParkData[1].name}</h2>
+                                <img src={popularParkData[1].images[1].url}/>
+                                <p>{popularParkData[1].description}</p>
+                                <Link className="button" to={`/explore/${popularParkData[1].parkCode}`}>View More Info</Link>
+                            </div>
+                        </div>
+
+                        <div class="column">
+                            <div class="card">
+                                <h2>{popularParkData[2].name}</h2>
+                                <img src={popularParkData[2].images[0].url}/>
+                                <p>{popularParkData[2].description}</p>
+                                <Link className="button" to={`/explore/${popularParkData[2].parkCode}`}>View More Info</Link>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="column">
+                            <div class="card">
+                                <h2>{popularParkData[3].name}</h2>
+                                <img src={popularParkData[3].images[2].url}/>
+                                <p>{popularParkData[3].description}</p>
+                                <Link className="button" to={`/explore/${popularParkData[3].parkCode}`}>View More Info</Link>
+                            </div>
+                        </div>
+
+                        <div class="column">
+                            <div class="card">
+                                <h2>{popularParkData[4].name}</h2>
+                                <img src={popularParkData[4].images[3].url}/>
+                                <p>{popularParkData[4].description}</p>
+                                <Link className="button" to={`/explore/${popularParkData[4].parkCode}`}>View More Info</Link>
+                            </div>
+                        </div>
+
+                        <div class="column">
+                            <div class="card">
+                                <h2>{popularParkData[5].name}</h2>
+                                <img src={popularParkData[5].images[4].url}/>
+                                <p>{popularParkData[5].description}</p>
+                                <Link className="button" to={`/explore/${popularParkData[5].parkCode}`}>View More Info</Link>
+                            </div>
+                        </div>
                     </div>
                 </div>
+                ) : <div><h2>Loading...</h2></div>}
 
-                <div class="column">
-                    <div class="card">
-                        <h2>Card 2</h2>
-                    </div>
-                </div>
+                
+            
 
-                <div class="column">
-                    <div class="card">
-                        <h2>Card 3</h2>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="column">
-                    <div class="card">
-                        <h2>Card 4</h2>
-                    </div>
-                </div>
-
-                <div class="column">
-                    <div class="card">
-                        <h2>Card 5</h2>
-                    </div>
-                </div>
-
-                <div class="column">
-                    <div class="card">
-                        <h2>Card 6</h2>
-                    </div>
-                </div>
+            <div>
+                <ReactJson src={popularParkData} theme="monokai" collapsed= {true} />
             </div>
 
         </div>
