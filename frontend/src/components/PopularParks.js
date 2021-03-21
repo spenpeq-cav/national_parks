48f70fcb0dbcfc5be0acd1cd228ed3948b0c27fa
Spencer Pequegnat <spencer.peq@gmail.com>
2021-03-21 12:52:38 -0700

    Image background shadow, popular parks card setup

--- frontend/src/components/PopularParks.js
+++ frontend/src/components/PopularParks.js
@@ -3,7 +3,47 @@ import React from 'react'
 function PopularParks() {
     return (
         <div>
-            <h1>Popular Parks</h1>
+
+            <div class="row">
+                <div class="column">
+                    <div class="card">
+                        <h2>Card 1</h2>
+                    </div>
+                </div>
+
+                <div class="column">
+                    <div class="card">
+                        <h2>Card 2</h2>
+                    </div>
+                </div>
+
+                <div class="column">
+                    <div class="card">
+                        <h2>Card 3</h2>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="column">
+                    <div class="card">
+                        <h2>Card 4</h2>
+                    </div>
+                </div>
+
+                <div class="column">
+                    <div class="card">
+                        <h2>Card 5</h2>
+                    </div>
+                </div>
+
+                <div class="column">
+                    <div class="card">
+                        <h2>Card 6</h2>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -8,12 +8,12 @@ function HomeScreen() {
     return (
         <section>
             <div className="main-container">
-                <img src="https://sgl-assets.imgix.net/files/article_hero/top-things-do-yosemite-national-park-via-magazine-shutterstock_758607316.jpg?h=61ce19a1&itok=bX4XJbAO?w=1200h=630&crop=faces,edges"></img>
                 <h1>All National Parks</h1>
-                <p>Explore information on all national parks. View activites, parking, and more!</p>
+                <p>Explore information on all national parks. View activites, ticket fees, parking, and more!</p>
                 <Link className="button" to="/explore">Explore Parks</Link>
             </div>
             <div className="popular-container">
+                <h1>Popular Parks</h1>
                 <PopularParks />
             </div>
         </section>
