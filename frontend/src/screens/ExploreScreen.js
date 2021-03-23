6998997f6254469f53822c79b9ff6a914e218a6e
Spencer Pequegnat <spencer.peq@gmail.com>
2021-03-23 12:54:56 -0700

    Explore screen card layout with data

--- frontend/src/screens/ExploreScreen.js
+++ frontend/src/screens/ExploreScreen.js
@@ -1,13 +1,13 @@
 import React, {useState, useEffect} from 'react';
-import ReactJson from 'react-json-view';
 import ReactPaginate from 'react-paginate';
 import axios from 'axios';
+import { Link } from 'react-router-dom'
 import '../components/css/ExploreScreen.css';
 
 function ExploreScreen() {
   const [offset, setOffset] = useState(0)
   const [data, setData] = useState([])
-  const [perPage] = useState(10)
+  const [perPage] = useState(8)
   const [pageCount, setPageCount] = useState(0)
 
   const base_url = 'https://developer.nps.gov/api/v1'
@@ -19,9 +19,13 @@ function ExploreScreen() {
     const data = res.data.data
     console.log(data)
     const slice = data.slice(offset, offset+perPage)
+    console.log(slice)
     const postData = slice.map(park => 
       <div key={park.id}>
-        <p>{park.fullName}</p>
+        <h5>{park.fullName}</h5>
+        <img src={park.images[0].url}></img>
+        <p>{park.description}</p>
+        <Link className="button-explore" to={`/explore/${park.parkCode}`}>More Info</Link>
       </div>)
     setData(postData)
     setPageCount(Math.ceil(data.length / perPage))
@@ -42,60 +46,53 @@ function ExploreScreen() {
   }, [offset])
 
   
-
-    // useEffect(() => {
-    //     fetch(url)
-    //         .then(res => res.json())
-    //         .then(res => {
-    //             setParkData(res.data)
-    //             setLoaded(true)})
-    //         .catch(error => console.log(error))
-        
-    //     console.log('Fetching data...')
-    // }, [])
-
-    
   return (
     <div>
-        {/* { loaded ? 
-            (<div>
-                <h1>All National Parks</h1>
-                <h2>{parkData[0].fullName}</h2>
-                <p>{parkData[0].description}</p>
-                <h4>States: {parkData[0].states}</h4>
-                <h4>{parkData[0].latLong}</h4>
-                <h4>{parkData[0].activities[0].name}</h4>
-                <ui>
-                    <h4>Activities:</h4>
-                    {parkData[0].activities.map((act) => 
-                        <li key={parkData[0].activities.index}>
-                        {act.name}
-                        </li>)
-                    }
-                </ui>
-            </div>)
-            : <h2>Loading...</h2> 
-        } */}
-      
-        
-      {data}
-        
-      
-      
-      
-      <ReactPaginate 
-        previousLabel={"prev"}
-        nextLabel={"next"}
-        breakLabel={"..."}
-        breakClassName={"break-me"}
-        pageCount={pageCount}
-        marginPagesDisplayed={2}
-        pageRangeDisplayed={5}
-        onPageChange={handlePageClick}
-        containerClassName={"pagination"}
-        subContainerClassName={"pages pagination"}
-        activeClassName={"active"} />
+      <div className='main-explore'>
+        <h2>Explore All National Park Services</h2>
+        <div className='container-explore'>
+          <div className='card-explore'>
+            {data[0]}
+          </div>
+          <div className='card-explore'>
+            {data[1]}
+          </div>
+          <div className='card-explore'>
+            {data[2]}
+          </div>
+          <div className='card-explore'>
+            {data[3]}
+          </div>
+        </div>
 
+        <div className='container-explore'>
+          <div className='card-explore'>
+            {data[4]}
+          </div>
+          <div className='card-explore'>
+            {data[5]}
+          </div>
+          <div className='card-explore'>
+            {data[6]}
+          </div>
+          <div className='card-explore'>
+            {data[7]}
+          </div>
+        </div>
+        <ReactPaginate 
+          previousLabel={"prev"}
+          nextLabel={"next"}
+          breakLabel={"..."}
+          breakClassName={"break-me"}
+          pageCount={pageCount}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          onPageChange={handlePageClick}
+          containerClassName={"pagination"}
+          subContainerClassName={"pages pagination"}
+          activeClassName={"active"} />
+      </div>
+ 
     </div>
   );
 }
