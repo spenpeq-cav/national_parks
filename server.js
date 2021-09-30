51cb8687ae2c6c6497e0a5fcc96c9aad9a615985
Spencer Pequegnat <spencer.peq@gmail.com>
2021-09-29 21:56:50 -0700

    Troubleshooting server

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require("express");
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.static(__dirname + 'frontend/build'));
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
 });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/frontend/build/index.html'));
+  });
   
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
