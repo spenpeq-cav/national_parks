8d9e615a7c13f004be471692ab4e4b34e1f295d6
Spencer Pequegnat <spencer.peq@gmail.com>
2022-05-29 19:16:17 -0700

    Moved PopularParks nps api call to server, auto format

--- frontend/src/components/PopularParks.js
+++ frontend/src/components/PopularParks.js
@@ -1,74 +1,79 @@
-import React, {useState, useEffect} from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-import ClipLoader from "react-spinners/ClipLoader"
+import ClipLoader from "react-spinners/ClipLoader";
 
 function PopularParks() {
+  const [popularParkData, setPopularParkData] = useState([]);
+  const [loaded, setLoaded] = useState(false);
 
-    const [popularParkData, setPopularParkData] = useState([])
-    const [loaded, setLoaded] = useState(false)
+  const getPopularData = async () => {
+    const res = await axios.get("/park_data_popular");
+    const data = res.data;
+    setPopularParkData(data);
+    setLoaded(true);
+  };
 
-    const popularParkCodes = 'yose,grca,yell'
+  useEffect(() => {
+    getPopularData();
+  }, []);
 
-    const base_url = 'https://developer.nps.gov/api/v1'
-    const endpoint_url = '/parks?parkCode='+ popularParkCodes +'&api_key=' + process.env.REACT_APP_API_KEY
-    const url = base_url + endpoint_url
-
-    const getPopularData = async() => {
-        const res = await axios.get(url)
-        const data = res.data.data
-        setPopularParkData(data)
-        setLoaded(true)
-    }
-
-    useEffect(() => {
-        getPopularData()
-        console.log("UseEffect Home")
-    }, [])
-
-    return (
-        <div>
-            
-            { loaded ? (
-                <div className="flex flex-col lg:grid lg:grid-cols-3 lg:gap-4">
-                   <div className="md:px-16 lg:px-2 w-full relative p-2">
-                        <Link className="" to={`/explore/${popularParkData[2].parkCode}`}>
-                            <div className="group">
-                                <img className="popular-home-card" src={popularParkData[2].images[0].url}/>
-                                <div className="popular-home-card-text top-2 right-6 md:right-20 lg:right-6">{popularParkData[2].name}</div>
-                            </div>
-                        </Link>   
-                    </div>
-                    <div className="md:px-16 lg:px-2 w-full relative p-2">
-                        <Link className="" to={`/explore/${popularParkData[0].parkCode}`}>
-                            <div className="group h-full">
-                                <img className="popular-home-card h-full" src={popularParkData[0].images[1].url}/>
-                                <div className="popular-home-card-text top-2 right-6 md:right-20 lg:right-6">{popularParkData[0].name}</div>
-                            </div>
-                        </Link>   
-                    </div>
-                    <div className="md:px-16 lg:px-2 w-full relative p-2">
-                        <Link className="" to={`/explore/${popularParkData[1].parkCode}`}>
-                            <div className="group h-full">
-                                <img className="popular-home-card h-full" src={popularParkData[1].images[0].url}/>
-                                <div className="popular-home-card-text top-2 right-6 md:right-20 lg:right-6">{popularParkData[1].name}</div>
-                            </div>
-                        </Link>   
-                    </div>
+  return (
+    <div>
+      {loaded ? (
+        <div className="flex flex-col lg:grid lg:grid-cols-3 lg:gap-4">
+          <div className="md:px-16 lg:px-2 w-full relative p-2">
+            <Link className="" to={`/explore/${popularParkData[2].parkCode}`}>
+              <div className="group">
+                <img
+                  className="popular-home-card"
+                  src={popularParkData[2].images[0].url}
+                />
+                <div className="popular-home-card-text top-2 right-6 md:right-20 lg:right-6">
+                  {popularParkData[2].name}
                 </div>
-                ) : 
-                    <div className="text-center">
-                        <div className="2xl:pb-52 2xl:px-12 w-full relative">
-                            <div className="text-center p-16">
-                                <ClipLoader color={"white"} size={150} />
-                            </div>
-                        </div>
-                    </div>
-                    }
-
+              </div>
+            </Link>
+          </div>
+          <div className="md:px-16 lg:px-2 w-full relative p-2">
+            <Link className="" to={`/explore/${popularParkData[0].parkCode}`}>
+              <div className="group h-full">
+                <img
+                  className="popular-home-card h-full"
+                  src={popularParkData[0].images[1].url}
+                />
+                <div className="popular-home-card-text top-2 right-6 md:right-20 lg:right-6">
+                  {popularParkData[0].name}
+                </div>
+              </div>
+            </Link>
+          </div>
+          <div className="md:px-16 lg:px-2 w-full relative p-2">
+            <Link className="" to={`/explore/${popularParkData[1].parkCode}`}>
+              <div className="group h-full">
+                <img
+                  className="popular-home-card h-full"
+                  src={popularParkData[1].images[0].url}
+                />
+                <div className="popular-home-card-text top-2 right-6 md:right-20 lg:right-6">
+                  {popularParkData[1].name}
+                </div>
+              </div>
+            </Link>
+          </div>
         </div>
-    )
+      ) : (
+        <div className="text-center">
+          <div className="2xl:pb-52 2xl:px-12 w-full relative">
+            <div className="text-center p-16">
+              <ClipLoader color={"white"} size={150} />
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default PopularParks
+export default PopularParks;
--- server.js
+++ server.js
@@ -1,205 +1,231 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
-const path = require('path');
-const mongoose = require('mongoose')
-const session = require("express-session")
-const passport = require("passport")
+const path = require("path");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
-const { authenticate } = require('passport');
+const { authenticate } = require("passport");
+const axios = require("axios");
 
 const PORT = process.env.PORT || 3001;
 
+const base_nps_url = "https://developer.nps.gov/api/v1";
+const api_url = "&api_key=" + process.env.NPS_API_KEY;
+
 const app = express();
 
-app.use(express.urlencoded({extended: true})); 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'frontend/build')));
+app.use(express.static(path.join(__dirname, "frontend/build")));
 
-app.use(session({
+app.use(
+  session({
     secret: process.env.SECRET,
     name: "natparks",
     cookie: {
-        httpOnly: true,
-        maxAge: 86400000
+      httpOnly: true,
+      maxAge: 86400000,
     },
     resave: false,
-    saveUninitialized: false
-}))
+    saveUninitialized: false,
+  })
+);
 
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 
-const DB_PASS = process.env.DB_PASS
-mongoose.connect("mongodb+srv://spenUser:" + DB_PASS + "@cluster0.jneic.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true})
+const DB_PASS = process.env.DB_PASS;
+mongoose.connect(
+  "mongodb+srv://spenUser:" +
+    DB_PASS +
+    "@cluster0.jneic.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
 const userSchema = new mongoose.Schema({
-    first: String,
-    last: String,
-    email: String,
-    password: String,
-    favorites: [String],
-})
+  first: String,
+  last: String,
+  email: String,
+  password: String,
+  favorites: [String],
+});
 
-userSchema.plugin(passportLocalMongoose)
+userSchema.plugin(passportLocalMongoose);
 
-const User = new mongoose.model("User", userSchema)
+const User = new mongoose.model("User", userSchema);
 
-passport.use(User.createStrategy())
-passport.serializeUser(User.serializeUser())
-passport.deserializeUser(User.deserializeUser())
-
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/api", (req, res) => {
-    if(req.isAuthenticated()){
-        res.json({ message: "Authenticated!" });
-    } else {
-        res.json({ message: "Not Authenticated!" });
-    }
+  if (req.isAuthenticated()) {
+    res.json({ message: "Authenticated!" });
+  } else {
+    res.json({ message: "Not Authenticated!" });
+  }
 });
 
-app.get("/user", function(req, res){
-    res.send(req.user)
-})
+app.get("/user", function (req, res) {
+  res.send(req.user);
+});
 
 app.get("/userauth", (req, res) => {
-    if(req.isAuthenticated()){
-        res.json({ auth: true });
-    } else {
-        res.json({ auth: false });
-    }
+  if (req.isAuthenticated()) {
+    res.json({ auth: true });
+  } else {
+    res.json({ auth: false });
+  }
 });
 
-app.get("/logout", function(req, res){
-    req.logOut()
-    req.session.destroy()
-    res.clearCookie("natparks", {
-        path: "/",
-        httpOnly: true, 
-        secure: "none",
-        sameSite: "none",    
-        expires: new Date(1), 
-    })
-    req.session.destroy()
-})
+app.get("/logout", function (req, res) {
+  req.logOut();
+  req.session.destroy();
+  res.clearCookie("natparks", {
+    path: "/",
+    httpOnly: true,
+    secure: "none",
+    sameSite: "none",
+    expires: new Date(1),
+  });
+  req.session.destroy();
+});
 
+app.get("/park_data_popular", async function (req, res) {
+  const popularParkCodes = "yose,grca,yell";
 
+  const endpoint_url = "/parks?parkCode=" + popularParkCodes;
+  const url = base_nps_url + endpoint_url + api_url;
 
+  const nps_res = await axios.get(url);
+  const data = nps_res.data.data;
 
-app.post("/checkFavorite", function(req, res){
-    var alreadyAFavorite = false
-    const parkcode = req.body.parkcode
+  res.send(data);
+});
 
-    if(req.isAuthenticated()){
-        User.findById(req.user._id, function(err, doc){
-            if(err){
-                console.log(err)
-            } else{
-                for(var i = 0; i < doc.favorites.length; i++){
-                    if(doc.favorites[i] == parkcode){
-                        alreadyAFavorite = true
-                        res.send({alreadyAFavorite: alreadyAFavorite, auth: true})
-                        break;
-                    }
-                }
-            }
-        })
-    } else {
-        res.send({alreadyAFavorite: false, auth: false});
+app.post("/checkFavorite", function (req, res) {
+  var alreadyAFavorite = false;
+  const parkcode = req.body.parkcode;
+
+  if (req.isAuthenticated()) {
+    User.findById(req.user._id, function (err, doc) {
+      if (err) {
+        console.log(err);
+      } else {
+        for (var i = 0; i < doc.favorites.length; i++) {
+          if (doc.favorites[i] == parkcode) {
+            alreadyAFavorite = true;
+            res.send({ alreadyAFavorite: alreadyAFavorite, auth: true });
+            break;
+          }
+        }
+      }
+    });
+  } else {
+    res.send({ alreadyAFavorite: false, auth: false });
+  }
+});
+
+app.post("/register", function (req, res) {
+  User.findOne({ username: req.body.username }, (err, doc) => {
+    if (err) {
+      console.log(err);
     }
-})
+    if (doc) {
+      res.send({ msg: "User with that email already exists" });
+    }
+    if (!doc) {
+      User.register(
+        {
+          username: req.body.username,
+          first: req.body.first,
+          last: req.body.last,
+        },
+        req.body.password,
+        function (err, user) {
+          if (err) {
+            console.log(err);
+            res.redirect("/register");
+          }
 
-app.post("/register", function(req, res){
-    User.findOne({username: req.body.username}, (err, doc) => {
-        if(err){
-            console.log(err)
+          passport.authenticate("local")(req, res, function () {
+            res.send(user);
+          });
+          // else {
+          //     res.redirect("/login")
+          //     passport.authenticate("local")(req, res, function(){
+          //         console.log(res)
+          //         res.redirect("/profile")
+          //     })
+          // }
         }
-        if(doc){
-            res.send({msg : "User with that email already exists"})
-        }
-        if(!doc){
-            User.register({username: req.body.username, first: req.body.first, last: req.body.last}, req.body.password, function(err, user){
-                if(err){
-                    console.log(err)
-                    res.redirect("/register")
-                }
+      );
+    }
+  });
+});
 
-                passport.authenticate("local")(req,res,function(){
-                    res.send(user)
-                })
-                // else {
-                //     res.redirect("/login")
-                //     passport.authenticate("local")(req, res, function(){
-                //         console.log(res)
-                //         res.redirect("/profile")
-                //     })
-                // }
-            })
+app.post("/login", function (req, res, next) {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      console.log(err);
+    }
+    if (!user) {
+      res.send({ msg: "No user with that email exist." });
+    } else {
+      req.logIn(user, (err) => {
+        if (err) {
+          console.log(err);
         }
-    })   
-})
-
-app.post("/login", function(req, res, next){
-    passport.authenticate("local", (err,user,info) => {
-        if(err) {
-            console.log(err)
-        }
-        if(!user){
-            res.send({msg: "No user with that email exist."})
-        } else {
-            req.logIn(user, err =>{
-                if(err) {
-                    console.log(err)
-                }
-                res.send({
-                    "user": {
-                        "username": user.username, 
-                        "first": user.first,
-                        "last": user.last,
-                        "favorites": user.favorites
-                }})
-            })
-        }
-    })(req, res, next)
-})
+        res.send({
+          user: {
+            username: user.username,
+            first: user.first,
+            last: user.last,
+            favorites: user.favorites,
+          },
+        });
+      });
+    }
+  })(req, res, next);
+});
 
 app.post("/favoriteAddOrRemove", (req, res) => {
-    const alreadyAFavorite = req.body.alreadyAFavorite
+  const alreadyAFavorite = req.body.alreadyAFavorite;
 
-    if(req.isAuthenticated()){
-        User.findById(req.user._id, function(err,user){
-            if(err){
-                console.log(err)
-            } else if(!alreadyAFavorite){
-                User.findByIdAndUpdate(
-                    req.user._id,
-                    { $push: { favorites : req.body.parkCode}},
-                    {upsert: true, new: true}, function(err, model) {
-                        console.log(err)
-                    }
-                )
-            } else{
-                User.findByIdAndUpdate(
-                    req.user._id,
-                    { $pull: { favorites : req.body.parkCode}},
-                    {upsert: true, new: true}, function(err, model) {
-                        console.log(err)
-                    }
-                )
-            }
-        })
-
-    } else {
-        res.send("Not Authenticated");
-    }
+  if (req.isAuthenticated()) {
+    User.findById(req.user._id, function (err, user) {
+      if (err) {
+        console.log(err);
+      } else if (!alreadyAFavorite) {
+        User.findByIdAndUpdate(
+          req.user._id,
+          { $push: { favorites: req.body.parkCode } },
+          { upsert: true, new: true },
+          function (err, model) {
+            console.log(err);
+          }
+        );
+      } else {
+        User.findByIdAndUpdate(
+          req.user._id,
+          { $pull: { favorites: req.body.parkCode } },
+          { upsert: true, new: true },
+          function (err, model) {
+            console.log(err);
+          }
+        );
+      }
+    });
+  } else {
+    res.send("Not Authenticated");
+  }
 });
 
-
-
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/frontend/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
 });
 
 app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
+  console.log(`Server listening on ${PORT}`);
 });
