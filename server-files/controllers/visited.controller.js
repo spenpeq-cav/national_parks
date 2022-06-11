6075df28c75471bfd860566e8dd2007511c09ed0
Spencer Pequegnat <spencer.peq@gmail.com>
2022-06-11 11:24:35 -0700

    addVisited functionality

--- server-files/controllers/visited.controller.js
+++ server-files/controllers/visited.controller.js
@@ -1,11 +1,27 @@
 const User = require("../models/user.models");
 
 function addVisited(req, res) {
-  //TODO
   const pc = req.body.pc;
-  console.log("Add visited");
-  console.log(pc);
-  return res.json({ code: pc });
+  if (req.isAuthenticated()) {
+    User.findById(req.user._id, (err, user) => {
+      if (err) {
+        console.log(err);
+        return res.send(err);
+      } else {
+        User.findByIdAndUpdate(
+          req.user._id,
+          { $push: { visited: pc } },
+          { upsert: true, new: true },
+          (err, model) => {
+            console.log(err);
+            return res.send(err);
+          }
+        );
+      }
+    });
+  } else {
+    return res.send("Not authenticated. Please Log in");
+  }
 }
 
 function removeVisited(req, res) {
